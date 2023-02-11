2d2709a70109dffcb295c5bff5cc44adb6825515
riajur <riajur@bitsmedia.com>
2023-02-12 02:03:08 +0800

    init commit

---
+++ TestGithubCICD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestGithubCICD
+//
+//  Created by Riajur Rahman on 12/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TestGithubCICDTests/TestGithubCICDTests.swift
@@ -0,0 +1,32 @@
+//
+//  TestGithubCICDTests.swift
+//  TestGithubCICDTests
+//
+//  Created by Riajur Rahman on 12/2/23.
+//
+
+import XCTest
+@testable import TestGithubCICD
+
+final class TestGithubCICDTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        XCTAssertEqual(2*2, 4)
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
